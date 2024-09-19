8c346aaa40e795f9367568b344b256a7d4cb3b91
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-09-19 18:41:08 +0900

    feat: update item component

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -27,14 +27,10 @@ function Item(props) {
     >
       <div className="Item-code">{props.item.code}</div>
       <div className="Item-title">
-        {props.item.title}{' '}
-        {count
-          ? ` | Выделяли ${count} ${plural(count, {
-              one: 'раз',
-              few: 'раза',
-              many: 'раз',
-            })}`
-          : ''}
+        {props.item.title}
+      </div>
+      <div className="Item-price">
+        {props.item.price}&nbsp;₽
       </div>
       <Button action={'Добавить'}></Button>
     </div>
